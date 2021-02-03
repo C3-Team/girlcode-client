6ebef4f16af8988b07a9a4365aa1c56aa59cee01
Nika Darab <nikadarab@pm.me>
2021-02-03 14:48:54 -0600

    heroku endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const configure = {
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://fierce-caverns-70893.herokuapp.com/api",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
 export default configure;
