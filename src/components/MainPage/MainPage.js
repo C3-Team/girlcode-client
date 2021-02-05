129e2c6e48adca179b993f28b163b029dfcfebc2
Nika Darab <nikadarab@pm.me>
2021-02-04 19:47:35 -0600

    change the background color

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,27 +5,43 @@ const MainPage = () => {
     <>
       <section>
         <div className="first">
-          <h3 className=" animate__animated animate__fadeIn">
-            Nearly 1 in 4 women in their menstruating years experience “period
-            poverty” ranging from the inability to purchase the products they
-            need to the inability to go to work, school and go on with their day
-            to day activities.
-          </h3>
+          <p className=" animate__animated animate__fadeIn">
+            {" "}
+            Nearly 1 in 4 women in their menstruating years experience{" "}
+            <b>“period poverty”</b> ranging from the inability to purchase the
+            products they need to the inability to go to work, school and go on
+            with their day to day activities.
+          </p>
         </div>
         <div className="second">
-          <h3 className=" animate__animated animate__fadeIn">
+          <p className=" animate__animated animate__fadeIn">
             GirlCode is a platform that matches individuals who need menstrual
             products to ones who can provide products based on location and
             urgency. Ask, and you shall receive!
-          </h3>
+          </p>
         </div>
         <div>
-          <h3 className="second animate__animated animate__pulse">
-            Post your <Link to="/needpage">Needs</Link> or{" "}
-            <Link to="/havepage">INVENTORIES</Link>, but before that, check out
-            what your neighbors <Link to="/needlist">NEED</Link> or
-            <Link to="/havelist"> HAVE</Link>.
-          </h3>
+          <p className="second animate__animated animate__pulse">
+            Post your{" "}
+            <Link to="/needpage">
+              <b>NEEDS</b>
+            </Link>{" "}
+            or{" "}
+            <Link to="/havepage">
+              {" "}
+              <b>INVENTORIES</b>{" "}
+            </Link>
+            , but before that, check out what your neighbors{" "}
+            <Link to="/needlist">
+              <b>NEEDS</b>
+            </Link>{" "}
+            or
+            <Link to="/havelist">
+              {" "}
+              <b>HAVE</b>
+            </Link>
+            .
+          </p>
         </div>
         {/* <div className="third">
           <div className="fourth">
