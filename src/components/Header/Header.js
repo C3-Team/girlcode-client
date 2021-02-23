4bd1f873434ebf1e2cc9c84f106d3d27a75fa323
Nika Darab <nikadarab@pm.me>
2021-02-23 13:33:54 -0600

    adding dog a,d users works

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,66 +2,64 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div>
-      <header className="header">
-        <nav>
-          <div>
-            <Link
-              style={{ textDecoration: "none" }}
-              to="/"
-              className="brand-logo"
-            >
-              {" "}
-              <h1 className="girlcode-logo">&nbsp;GirlCode</h1>
-            </Link>
-            <ul className="nav-links dropdown">
-              <button className="dropbtn">Needs</button>
-              <div className="dropdown-content">
-                <div>
-                  <Link style={{ textDecoration: "none" }} to="/needlist">
-                    List of Needs
-                  </Link>
-                </div>
-                <div>
-                  <Link style={{ textDecoration: "none" }} to="/needpage">
-                    {" "}
-                    Post a need
-                  </Link>
-                </div>
+    <header className="header">
+      <nav>
+        <div>
+          <Link
+            style={{ textDecoration: "none" }}
+            to="/"
+            className="brand-logo"
+          >
+            {" "}
+            <h1 className="girlcode-logo">&nbsp;GirlCode</h1>
+          </Link>
+          <ul className="nav-links dropdown">
+            <button className="dropbtn">Needs</button>
+            <div className="dropdown-content">
+              <div>
+                <Link style={{ textDecoration: "none" }} to="/needlist">
+                  List of Needs
+                </Link>
               </div>
-            </ul>
-            <ul className="nav-links dropdown">
-              <button className="dropbtn">Inventories</button>
-              <div className="dropdown-content">
-                <div>
-                  <Link style={{ textDecoration: "none" }} to="/havelist">
-                    List of Inventories
-                  </Link>
-                </div>
-                <div>
-                  <Link style={{ textDecoration: "none" }} to="/havepage">
-                    Post an inventory
-                  </Link>
-                </div>
+              <div>
+                <Link style={{ textDecoration: "none" }} to="/needpage">
+                  {" "}
+                  Post a need
+                </Link>
               </div>
-            </ul>
-            <ul className="nav-links ">
-              <button className="dropbtn">
-                <div className=" donatebtn animate__animated animate__swing ">
-                  <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://checkout.square.site/merchant/MLZMPESY4AYJD/checkout/TEOJREQNW4MEKLQ7EPCZELUF"
-                  >
-                    Donate
-                  </a>
-                </div>
-              </button>
-            </ul>
-          </div>
-        </nav>
-      </header>
-    </div>
+            </div>
+          </ul>
+          <ul className="nav-links dropdown">
+            <button className="dropbtn">Inventories</button>
+            <div className="dropdown-content">
+              <div>
+                <Link style={{ textDecoration: "none" }} to="/havelist">
+                  List of Inventories
+                </Link>
+              </div>
+              <div>
+                <Link style={{ textDecoration: "none" }} to="/havepage">
+                  Post an inventory
+                </Link>
+              </div>
+            </div>
+          </ul>
+          <ul className="nav-links ">
+            <button className="dropbtn">
+              <div className=" donatebtn animate__animated animate__swing ">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://checkout.square.site/merchant/MLZMPESY4AYJD/checkout/TEOJREQNW4MEKLQ7EPCZELUF"
+                >
+                  Donate
+                </a>
+              </div>
+            </button>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -3,66 +3,42 @@ import { Link } from "react-router-dom";
 const MainPage = () => {
   return (
     <>
-      <section>
-        <div className="first">
-          <p className=" animate__animated animate__fadeIn">
+      <section className="main">
+        <p className=" animate__animated animate__fadeIn">
+          {" "}
+          Nearly 1 in 4 women in their menstruating years experience{" "}
+          <b>“period poverty”</b> ranging from the inability to purchase the
+          products they need to the inability to go to work, school and go on
+          with their day to day activities.
+        </p>
+        <p className=" animate__animated animate__fadeIn">
+          GirlCode is a platform that matches individuals who need menstrual
+          products to ones who can provide products based on location and
+          urgency. Ask, and you shall receive!
+        </p>
+        <p className="animate__animated animate__pulse">
+          Post your{" "}
+          <Link to="/needpage">
+            <b>NEEDS</b>
+          </Link>{" "}
+          or{" "}
+          <Link to="/havepage">
             {" "}
-            Nearly 1 in 4 women in their menstruating years experience{" "}
-            <b>“period poverty”</b> ranging from the inability to purchase the
-            products they need to the inability to go to work, school and go on
-            with their day to day activities.
-          </p>
-        </div>
-        <div className="second">
-          <p className=" animate__animated animate__fadeIn">
-            GirlCode is a platform that matches individuals who need menstrual
-            products to ones who can provide products based on location and
-            urgency. Ask, and you shall receive!
-          </p>
-        </div>
-        <div>
-          <p className="second animate__animated animate__pulse">
-            Post your{" "}
-            <Link to="/needpage">
-              <b>NEEDS</b>
-            </Link>{" "}
-            or{" "}
-            <Link to="/havepage">
-              {" "}
-              <b>INVENTORIES</b>{" "}
-            </Link>
-            , but before that, check out what your neighbors{" "}
-            <Link to="/needlist">
-              <b>NEEDS</b>
-            </Link>{" "}
-            or
-            <Link to="/havelist">
-              {" "}
-              <b>HAVE</b>
-            </Link>
-            .
-          </p>
-        </div>
-        {/* <div className="third">
-          <div className="fourth">
-            <h3>
-              <Link style={{ textDecoration: "none" }} to="needpage">
-                I,need
-              </Link>
-            </h3>
-          </div>
-          <div className="fourth">
-            <h3>
-              <Link style={{ textDecoration: "none" }} to="havepage">
-                I,have
-              </Link>
-            </h3>
-          </div>
-        </div> */}
+            <b>INVENTORIES</b>{" "}
+          </Link>
+          , but before that, check out what your neighbors{" "}
+          <Link to="/needlist">
+            <b>NEEDS</b>
+          </Link>{" "}
+          or
+          <Link to="/havelist">
+            {" "}
+            <b>HAVE</b>
+          </Link>
+          .
+        </p>
       </section>
-
       <br />
-
       <footer>&copy; 2021 GirlCode</footer>
     </>
   );
